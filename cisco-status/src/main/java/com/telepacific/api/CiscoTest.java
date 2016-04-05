be23e71546b00a30a2fa0f27f336e1299f32b912
berndhopp <bernd@vaadin.com>
2016-04-05 20:02:43 +0200

    added unittest to try out cisco api

---
+++ cisco-status/src/main/java/com/telepacific/api/CiscoTest.java
@@ -0,0 +1,14 @@
+package com.telepacific.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CiscoTest {
+
+    @Test
+    public void foo(){
+        //TODO cisco playground here
+          System.out.print(true);
+    }
+}
