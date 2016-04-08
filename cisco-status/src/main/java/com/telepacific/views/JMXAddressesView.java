9f5bd69be7d77561f34ff310f60b1739a0b95832
berndhopp <bernd@vaadin.com>
2016-04-08 15:14:58 +0200

    overhauled addresses view

--- cisco-status/src/main/java/com/telepacific/views/JMXAddressesView.java
+++ cisco-status/src/main/java/com/telepacific/views/JMXAddressesView.java
@@ -2,22 +2,32 @@ package com.telepacific.views;
 
 import com.google.inject.Inject;
 
+import com.telepacific.components.DeviceSelector;
 import com.telepacific.components.JMXAddressesGrid;
 import com.vaadin.guice.annotation.GuiceView;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.Panel;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 @GuiceView(name = "jmx-addresses-view")
-public class JMXAddressesView extends Panel implements View {
+public class JMXAddressesView extends VerticalLayout implements View {
+
+    private final DeviceSelector deviceSelector;
+
+    private final Label topLabel = new Label();
 
     @Inject
-    public JMXAddressesView(JMXAddressesGrid jmxAddressesGrid) {
-        setContent(jmxAddressesGrid);
+    public JMXAddressesView(JMXAddressesGrid jmxAddressesGrid, DeviceSelector deviceSelector) {
+        this.deviceSelector = deviceSelector;
         setSizeFull();
+        addComponents(topLabel, jmxAddressesGrid);
+        setExpandRatio(jmxAddressesGrid, 1);
+        setMargin(true);
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
+        topLabel.setValue("jmx-addresses for " + deviceSelector.getValue());
     }
 }
